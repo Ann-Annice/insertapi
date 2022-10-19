bdb9215ee7cea322e6f2a4e37709ef0701f34f53
Ann-Annice <annannice98@gmail.com>
2022-10-19 09:10:51 +0530

    insertapi

---
+++ src/main/java/com/nestdigital/Controller/StudentController.java
@@ -0,0 +1,4 @@
+package com.nestdigital.Controller;
+
+public class StudentController {
+}
---
+++ src/test/java/com/nestdigital/Xyzbackend/XyzbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nestdigital.Xyzbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XyzbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
